055e0781e0e7cdb1063a48d0b4b5075c58db4c2b
Andrew Karn <andrewkarn@Andrews-MacBook-Pro-2.local>
2020-04-28 20:46:27 -0700

    final

--- js/app.js
+++ js/app.js
@@ -1,82 +1,84 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
-const ulist = document.getElementById('navbar__list');
+const ulist = document.getElementById("navbar__list");
 const containers = document.getElementsByClassName("landing__container");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-function getContainerIds(htmlObject, list=[]) {
-    for (container of htmlObject) {
-        list.push(container.parentNode.id);
-    }
-    return list;
+ *
+ */
+function getContainerIds(htmlObject, list = []) {
+  for (container of htmlObject) {
+    list.push(container.parentNode.id);
+  }
+  return list;
 }
 
-
-function constructAnchorLinks (containerIds) {
-    let list = [];
-    let idx = 0;
-    let buttonIds = [];
-    for (id of containerIds) {
-        let newItem = document.createElement("li");
-        let newText = document.createTextNode(`#${id}`);
-        newItem.setAttribute("id", `list-item-${idx}`);
-        newItem.setAttribute("class", "navbar__menu li");
-        newItem.appendChild(newText);
-        // let button = `${id}-button`
-        ulist.appendChild(newItem);
-        idx += 1
-    }
+function constructAnchorLinks(containerIds) {
+  let idx = 0;
+  for (id of containerIds) {
+    let section = document.getElementById(id);
+    let newItem = document.createElement("li");
+    newItem.innerHTML = `<button class="navbar__menu li" type="button">${id}</button>`;
+    newItem.addEventListener('click', () => {
+      section.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+    ulist.appendChild(newItem);
+    idx += 1;
+  }
 }
-
+// This method was taught in an article btw, to give credit where due:
+// https://vanillajstoolkit.com/helpers/isinviewport/
+function isInViewport(element) {
+  let rect = element.getBoundingClientRect();
+  let html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+}
+// listen for all scrolls and call the isInViewport method.
+function addListenersToContainers(containerIds) {
+  for (container of containerIds) {
+    let section = document.getElementById(container);
+    window.addEventListener(
+      "scroll",
+      () => {
+        isInViewport(section) ? section.classList.add("your-active-class") : section.classList.remove("your-active-class");
+      },
+      false
+    );
+  }
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
-const ids = getContainerIds(containers)
+ *
+ */
+const ids = getContainerIds(containers);
 constructAnchorLinks(ids);
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+addListenersToContainers(ids);
